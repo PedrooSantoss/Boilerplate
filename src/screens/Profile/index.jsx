ab68a3a0616630407d9ac40b19abcb2811f58685
unknown <pedro.miguel@aluno.senai.br>
2024-03-13 15:27:02 -0300

    feat: create profile screen

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import styles from './style'
 import Title from '../../components/Title'
 
-export default function Home() {
+export default function Profile() {
   return (
     <View style={styles.create}>
       <Title title="Create a new account" />
